e1cf5a61b80e15f67e85ece6e4e49308b14d1521
Alex Zinchenko <admloki@gmail.com>
2012-01-11 08:35:12 +0400

    Added bootstrap to install dependencies

---
+++ bootstrap.rb
@@ -0,0 +1,16 @@
+require 'fileutils'
+
+if File.exists?('github.gemspec')
+  spec = eval(File.read('github.gemspec'))
+  
+  spec.runtime_dependencies.each do |dep|
+    begin
+      cmd = "`which gem` install #{dep.name} --no-ri --no-rdoc --version='#{dep.requirement}'"
+      system(cmd)
+    rescue
+      raise "Execution failed with '#{cmd}' command"
+    end
+  end
+else
+  raise "No gem specification found"
+end
