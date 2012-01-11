ecc1884c1c96242cdeeb5dc95a8a576d93214a17
Alex Zinchenko <admloki@gmail.com>
2012-01-11 08:50:19 +0400

    Fix #2

--- bootstrap.rb
+++ bootstrap.rb
@@ -1,12 +1,17 @@
 require 'rubygems'
-require 'fileutils'
+require 'stringio'
 
 if File.exists?('github.gemspec')
+  stdout, stderr = $stdout, $stderr
+  $stdout, $stderr = StringIO.new, StringIO.new
+  
   spec = eval(File.read('github.gemspec'))
+  
+  $stdout, $stderr = stdout, stderr
   
   spec.runtime_dependencies.each do |dep|
     begin
-      cmd = "`which gem` install #{dep.name} --no-ri --no-rdoc --version='#{dep.requirement}'"
+      cmd = "`which gem` install #{dep.name} --no-ri --no-rdoc --no-update-sources --version='#{dep.requirement}'"
       system(cmd)
     rescue
       raise "Execution failed with '#{cmd}' command"
